636e0caaa0bc617698501f7e7d3268492ba05a8e
bewzunjo <saran_kim@cmu.ac.th>
2022-09-22 16:01:01 +0700

    finished

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -6,7 +6,7 @@ export default function roomIdMessageRoute(req, res) {
     const rooms = readDB();
     const roomId = req.query.roomId;
     const idx = rooms.findIndex((x) => x.roomId === roomId);
-    //the room does not exist
+    
     if (idx === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
 
@@ -15,21 +15,21 @@ export default function roomIdMessageRoute(req, res) {
     const rooms = readDB();
     const roomId = req.query.roomId;
 
-    //read request body
+    
     const text = req.body.text;
 
-    //create new id
+    
     const newId = uuidv4();
 
     const idx = rooms.findIndex((x) => x.roomId === roomId);
-    //the room does not exist
+    
     if (idx === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
-    //invalid text input
+  
     if (typeof text !== "string")
       return res.status(400).json({ ok: false, message: "Invalid text input" });
 
-    //valid request
+   
     const message = { messageId: newId, text };
     rooms[idx].messages.push(message);
     writeDB(rooms);
